1f99174b4dc5b8f1416bacb478fa84d7450ec778
Tran Minh Nghĩa <tranminhnghia.a01@gmail.com>
2025-07-03 13:42:33 +0700

    demo state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
+import { logo } from "./logo.svg";
 import "./App.css";
-// import { useDispatch, useSelector } from "react-redux";
-// import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./components/MyComponent";
-const App = () => {
-  // const count = useSelector((state) => state.counter.count);
-  // const dispatch = useDispatch();
 
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>Hello world!</p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-    <>
-      <h1>Hello World!</h1>
-      <MyComponent />
-    </>
-  );
-};
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    name: "Nghia",
+    address: "Minh Nghia 11a1",
+    age: 25,
+  };
+
+  handleClick(event) {
+    console.log("My name is", this.state.name);
+    this.setState({ name: "Eric", age: Math.floor(Math.random() * 100) });
+  }
+  render() {
+    return (
+      <>
+        My name is {this.state.name}, age {this.state.age}
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me!
+        </button>
+      </>
+    );
+  }
+}
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello world!</p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// };
 
 export default App;
