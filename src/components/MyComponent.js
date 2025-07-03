0310049b41e58cdfa7ccdcb581e247c4ecf05633
Tran Minh Nghĩa <tranminhnghia.a01@gmail.com>
2025-07-03 12:57:32 +0700

    Demo Class Components

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <>
+        Demo Class
+        {Math.random()}
+      </>
+    );
+  }
+}
+
+export default MyComponent;
